07e1ffa102f5342f3dded440f7daf7e8060b83ab
mellzamora <mellzamora@outlook.com>
2016-01-04 13:15:11 +0800

    updated

--- Resources/public/js/rz/login-form.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-    rz_user_uniform_checkbox.init();
-});
-
-var rz_user_uniform_checkbox = {
-    init: function() {
-        jQuery(".uni_style_checkbox").uniform();
-    }
-}
