e9ccfc93d8d216e8556d793c17b184da270f2ff4
unknown <diszo.sasil@MANMOVNTB7705.ad.insidemedia.net>
2016-07-08 16:12:14 +0800

    update

--- Entity/UserManager.php
+++ Entity/UserManager.php
@@ -47,5 +47,20 @@ class UserManager extends BaseUserManager
             return false;
         }
     }
+	
+	public function fetchRegistrationCount2($resultCount = 5) {
+
+        $qb = $this->repository->createQueryBuilder('u');
+        $em = $qb->getEntityManager();
+        $query = $em->createQuery("SELECT COUNT(u.createdAt) as registrationCount, DATE_FORMAT(u.createdAt, '%Y-%m-%d') as registerDate  FROM ".$this->class." u WHERE u.createdAt >= '".$dateFrom."' AND u.createdAt <= '".$dateTo."' GROUP BY registerDate ORDER BY u.createdAt ASC")
+                    ->setMaxResults($resultCount);
+
+        try {
+            return $query->getResult();
+        } catch(\Exception $e) {
+            throw $e;
+            return false;
+        }
+    }
 
 }
